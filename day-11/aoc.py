1dec08a6b4aa9117af62e7ca3164f5874773f495
Edoardo Manenti <edoardomanenti99@gmail.com>
2025-12-11 08:37:42 +0100

    day 11 solved

---
+++ day-11/aoc.py
@@ -0,0 +1,110 @@
+import timeit
+import unittest
+from collections import defaultdict
+
+class AocSolver():
+    tree = defaultdict(list)
+
+    def setup(self, input_file_path = 'input.txt') -> str:
+        return open("input.txt", "r").read()
+
+    def bfs(self, curr, end, dp):
+        if curr in dp.keys():
+            return dp[curr], dp
+        if curr == end:
+            return 1, dp
+        sum = 0
+        for n in self.tree[curr]:
+            s, dp = self.bfs(n, end, dp)
+            sum += s
+        dp[curr] = sum
+        return sum, dp
+
+    def part_one(self, input: str) -> int:
+        for line in input.strip().splitlines():
+            key, values = line.split(':')
+            self.tree[key.strip()] = values.strip().split(' ')
+
+        v, _ =  self.bfs('you', 'out', {})
+        return v
+
+    def part_two(self, input: str) -> int:
+        for line in input.strip().splitlines():
+            key, values = line.split(':')
+            self.tree[key.strip()] = values.strip().split(' ')
+
+        svr_dac,_ = self.bfs('svr', 'dac', {})
+        svr_fft,_ = self.bfs('svr', 'fft', {})
+        dac_out,_ = self.bfs('dac', 'out', {})
+        dac_fft,_ = self.bfs('dac', 'fft', {})
+        fft_dac,_ = self.bfs('fft', 'dac', {})
+        fft_out,_ = self.bfs('fft', 'out', {})
+
+        return svr_dac*dac_fft*fft_out + svr_fft*fft_dac*dac_out
+
+class AocSolutionTest(unittest.TestCase):
+    def setUp(self):
+        self.test_part_one = """
+aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+        """
+        self.solution_part_one = 5
+
+        self.test_part_two = """
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+        """
+        self.solution_part_two = 2
+
+        self.solver = AocSolver()
+
+    def test_part_one(self):
+        value = self.solver.part_one(self.test_part_one)
+
+        self.assertIsNotNone(value)
+        self.assertEqual(value, self.solution_part_one)
+
+    def test_part_two(self):
+        value = self.solver.part_two(self.test_part_two)
+
+        self.assertIsNotNone(value)
+        self.assertEqual(value, self.solution_part_two)
+
+if __name__ == '__main__':
+    setup = '''
+from aoc import AocSolver
+solver = AocSolver()
+problem = solver.setup()
+    '''
+    iterations = 1
+    print(f"Time for part1: {
+    timeit.timeit(setup=setup, 
+                  stmt='print(solver.part_one(problem))', 
+                  number = iterations)
+                  /iterations
+    } sec")
+    print(f"Time for part2: {
+    timeit.timeit(setup=setup, 
+                  stmt='print(solver.part_two(problem))', 
+                  number = iterations)
+                  /iterations
+    } sec")
